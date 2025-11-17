85101028bda08a8e330f49e91731858052c1945b
Pepper-926 <gaboland1405@gmail.com>
2025-11-17 16:17:32 -0600

    Se dividio el proyecto en varios chunks para reducir tiempos de carga

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  build: {
+    chunkSizeWarningLimit: 600, // opcional
+
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          mui: [
+            '@mui/material',
+            '@mui/system',
+            '@emotion/react',
+            '@emotion/styled',
+          ],
+          motion: ['framer-motion'],
+          recaptcha: ['react-google-recaptcha-v3'],
+        }
+      }
+    }
+  }
+});
